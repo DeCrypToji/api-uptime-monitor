aec14dcf7123d608b8328f813e7640818e95a6c4
Janali Miller-Reid <janalimiller2@gmail.com>
2026-06-26 13:13:00 +0100

    fix: remove unused axios import from ForgotPassword

--- frontend/src/pages/ForgotPassword.tsx
+++ frontend/src/pages/ForgotPassword.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios'
+
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState('')
